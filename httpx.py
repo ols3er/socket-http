6bfd31e0571faa4f0c937e2adb6d82946bf628f8
YiDa.Z <thisforeda@live.cn>
2016-07-14 22:36:43 +0800

    修复一处错误
    当传输方式chunked且content-encoding未定义时直接了CHUNKED数据

--- httpx.py
+++ httpx.py
@@ -13,7 +13,7 @@ from urllib import parse
 #import zlib
 
 
-__version__ = '2.1.2.2016613'
+__version__ = '2.1.3.2016714'
 
 #-------------------------------------------------------------------------------------#
 #                   (C) 2015 by ZhangYiDa <http://www.loogi.cn>                       #
@@ -131,8 +131,7 @@ class ResponseHandler:
                 return UNKNOWN_ERROR
 
         if self.__transfer_type_is_chunked:
-            __data = self.__process_chunked_blocks(self.__buffer)
-            if __data is None:
+            if not self.__process_chunked_blocks(self.__buffer):
                 return DECODE_ERROR
 
         if self.__is_content_encoded_packet:
@@ -145,23 +144,12 @@ class ResponseHandler:
                 content_encode_processor = self.__default_content_encode_processor
             
             try :
-                if self.__transfer_type_is_chunked:
-                    self.__buffer = content_encode_processor(__data)
-                else:
-                    self.__buffer = content_encode_processor(self.__buffer)
+                self.__buffer = content_encode_processor(self.__buffer)
                 # < all done >
             except Exception as err:
                 self.__recorder.write('解码时发生错误,编码格式为 %s'%
                         self.__http_header('Content-Encoding'),Exception = err)
                 return DECODE_ERROR
-        '''
-        # 此部分被注释,
-        else:
-            # 数据没有进行编码,但是为了安全还是看一下数据是否用CHUNKED传输的(应该是不可能的一步)
-            if self.__transfer_type_is_chunked:
-                self.__buffer = __data
-                # < all done >
-        '''
         return True
         
     def __process_chunked_blocks(self,data):
@@ -177,7 +165,8 @@ class ResponseHandler:
             else:
                 self.__recorder.write('Error during merge chunked blocks')
                 return None
-        return __cat_buffer
+        self.__buffer = __cat_buffer
+        return True
 
     def __default_content_encode_processor(self,data):
         return data
@@ -319,21 +308,21 @@ __response_handler = ResponseHandler()
 
 MAX_REDIRECT_LOOP = 8
 
-def urlopen(url,data_dict = None,
-                    custom_header = None,
+def urlopen(url,postdata = None,
+                    header = None,
                             callback = None,
-                                     follow_redirect = -1):
-    ''' default Method is GET. if data_dict is provided Method will be POST
-        custom_header is a dict like {'Accept-Encoding':'what',,,}
-        if follow_redirect = 1,urlopen will try to follow the new location
+                                     redirect = -1):
+    ''' default Method is GET. if postdata is provided Method will be POST
+        header is a dict like {'Accept-Encoding':'what',,,}
+        if redirect = 1,urlopen will try to follow the new location
     '''
     if not is_valid_url(url):
         return HTTP_INVALID_URL
     
     __temp_headers = __base_headers.copy()
 
-    if custom_header :
-        for key,value in custom_header.items():
+    if header :
+        for key,value in header.items():
             __temp_headers.update({key:value})
 
     __cur_protocol = get_protocol(url)
@@ -350,9 +339,9 @@ def urlopen(url,data_dict = None,
     if new_addr != __base_connector.curr_addr():
         __base_connector.reset_addr(new_addr)
     
-    __is_post = True if data_dict else False
-    if data_dict is not None:
-        __post_data = parse.urlencode(data_dict)    
+    __is_post = True if postdata else False
+    if postdata is not None:
+        __post_data = parse.urlencode(postdata)    
     
     __temp_headers['Host'] = get_host_str(url)
     if __is_post:
@@ -378,7 +367,7 @@ def urlopen(url,data_dict = None,
         return retval
 
     if __response_handler.http_header('statuscode') in ('301','302','303','307'):
-        if follow_redirect != -1 and follow_redirect < MAX_REDIRECT_LOOP:
+        if redirect != -1 and redirect < MAX_REDIRECT_LOOP:
             __new_url = __response_handler.http_header('location')
             if not __new_url:
                 return None
@@ -391,21 +380,10 @@ def urlopen(url,data_dict = None,
                 __custom_header.update({"Cookie":cookie})
                 
             return urlopen(__new_url,
-                           custom_header = __custom_header,
+                           header = __custom_header,
                            callback=callback,
-                           follow_redirect = follow_redirect + 1
+                           redirect = redirect + 1
                            )
     return __response_handler
-
-
-
-
-
-
-
-
-
-
-
 
 
